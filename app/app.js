36f67412a4f078d4d7b7555ef85e6bd9fbe82219
Chen Li <yichen.li0830@gmail.com>
2017-11-09 14:38:38 -0800

    Handle response and error properly and remove local storage of the wav file after attempting to save to OSS

--- app/app.js
+++ app/app.js
@@ -3,31 +3,31 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-// const convertAudioToWav = require('./convertAudioToWav')
 const handler = require('./handler')
 
 // parse body
-app.use(bodyParser.json())       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
 
 app.get('/', (req, res) => res.send('Audio Formatting Service is up :D! Make use of it by posting with param \'url\''))
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   if (!req.body.url) {
     return res.send('No audio url!')
   }
+  // console.log('handler(req.body.url)', handler(req.body.url))
   handler(req.body.url)
-  // convertAudioToWav(req.body.url)
-  //   .then(result => {
-  //     console.log('result', result)
-  //   })
-//   console.log('req', req)
-//   console.log('req', Object.keys(req))
-//   console.log('req.body', req.body)
-//   console.log('req.params', req.params)
-  res.send('Got a POST request')
+    .then(savedUrl => {
+      console.log(`audio file is processed and saved to ${savedUrl}`)
+      res.send(`Success: Audio file is formatted and saved ${savedUrl}`)
+      // return savedUrl
+    })
+    .catch(err => {
+      console.error('err', err)
+      next(err)
+    })
 })
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`App listening on port ${port}!`))
--- app/saveWavToOSS.js
+++ app/saveWavToOSS.js
@@ -1,8 +1,21 @@
-const saveWavToOSS = (path) => {
-  console.log('saveWavToOSS start')
-//   return 'saveWavToOSS'
-  let url = '123'
-  return url
+const ossClient = require('./ossClient')
+let path = require('path')
+const fs = require('fs')
+
+const saveWavToOSS = (filePath) => {
+  console.log('saveWavToOSS path', filePath)
+  let fileName = path.basename(filePath)
+  return ossClient.put(fileName, filePath)
+    .then(result => {
+      // remove local wav file
+      fs.unlinkSync(filePath)
+
+      if (result.res.statusCode === 200) {
+        return result.url
+      } else {
+        throw new Error('file is not saved successfully')
+      }
+    })
 }
 
 module.exports = saveWavToOSS
