4648339b873f2df62c6cbf8fc22ace3ea775f48b
Chen Li <yichen.li0830@gmail.com>
2017-11-09 16:06:15 -0800

    Remove trivial comments

--- app/ossClient.js
+++ app/ossClient.js
@@ -1,8 +1,6 @@
 var OSS = require('ali-oss').Wrapper
 
 var client = new OSS({
-// Todo: use external variables
-//   region: 'oss-us-west-1',
   region: process.env.REGION,
   bucket: process.env.BUCKET,
   accessKeyId: process.env.ACCESS_KEY_ID,
