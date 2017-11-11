c653eeb88f6304c455d27ee9ddf84489945baf0b
Chen Li <yichen.li0830@gmail.com>
2017-11-10 22:04:33 -0800

    Add check for if the audio file url exists

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -1,11 +1,20 @@
 const convertAudioToWav = require('../convertAudioToWav')
 const saveWavToOSS = require('../saveWavToOSS')
+const {urlExists} = require('./helper')
 
 const formatAudioFile = ({audioUrl, encoding, channel, sampleRate}) => {
-  return convertAudioToWav({filePath: audioUrl, encoding, channel, sampleRate})
-    .then(path => {
-      console.log('path', path)
-      return saveWavToOSS(path)
+  // check if url exist
+  return urlExists(audioUrl)
+    .then(exists => {
+      if (!exists) throw new Error('audioUrl doesn\'t exist')
+    })
+    .then(() => {
+      // format audio
+      return convertAudioToWav({filePath: audioUrl, encoding, channel, sampleRate})
+      .then(path => {
+        console.log('path', path)
+        return saveWavToOSS(path)
+      })
     })
 }
 
